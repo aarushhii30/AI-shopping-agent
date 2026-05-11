87314e6fc26676df289bbeb2e63db578709e749d
aarushhii30 <aarushhiisharma@gmail.com>
2026-05-11 12:58:47 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import { CartProvider } from './context/CartContext';
 import { CheckoutProvider } from './context/CheckoutContext';
+
+import AuthPage from './pages/AuthPage';
 import ChatPage from './pages/ChatPage';
 import CartPage from './pages/CartPage';
 import CheckoutPage from './pages/CheckoutPage';
@@ -12,12 +15,15 @@ function App() {
     <BrowserRouter>
       <CartProvider>
         <CheckoutProvider>
+
           <Routes>
-            <Route path="/"         element={<ChatPage />} />
-            <Route path="/cart"     element={<CartPage />} />
+            <Route path="/" element={<AuthPage />} />
+            <Route path="/chat" element={<ChatPage />} />
+            <Route path="/cart" element={<CartPage />} />
             <Route path="/checkout" element={<CheckoutPage />} />
             <Route path="/order-success" element={<OrderSuccessPage />} />
           </Routes>
+
         </CheckoutProvider>
       </CartProvider>
     </BrowserRouter>
